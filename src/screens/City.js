09f05dd685195632b6f3493abdf40b8c081c79ea
pphila <efl2ice89@gmail.com>
2023-08-22 14:38:32 -0700

    move currentweather and upcomingweather component into screens directory and created ListItem component

---
+++ src/screens/City.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { SafeAreaView, Text, StyleSheet } from 'react-native';
+
+const City = () => {
+  return (
+    <SafeAreaView>
+      <Text>City</Text>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  
+})
+export default City;
