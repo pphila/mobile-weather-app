47388f7fc0199b51afacfdc3df53f012fe823cd8
pphila <efl2ice89@gmail.com>
2023-08-22 10:19:26 -0700

    add hardcoded structure for weather app

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import  { View, Text, SafeAreaView, StyleSheet } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={styles.wrapper}>
+      
+      <View style= {styles.container}>
+        <Text>Current Weather</Text>
+        <Text style= {styles.temp}>6</Text>
+        <Text style = {styles.feels}>Feels like 5</Text>
+        <View style = {styles.highLowWrapper}>
+          <Text style = {styles.highLow}>High: 8</Text>
+          <Text style = {styles.highLow}>Low: 6</Text>
+        </View>
+      </View>
+      
+      <View style= {styles.bodyWrapper}>
+        <Text>It's Sunny</Text>
+        <Text>It's perfect T-shirt weather</Text>
+      </View>
+      
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: 'pink',
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50
   },
-});
+  wrapper: {
+    flex: 1
+  },
+  temp: {
+    color: 'black',
+    fontSize: 48
+  },
+  feels: {
+    color: 'black',
+    fontSize: 30
+  },
+  highLow: {
+    color: 'black',
+    fontSize: 20
+  },
+  highLowWrapper: {
+    flexDirection: 'row'
+  },
+  bodyWrapper: {
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start'
+  }
+})
+export default App;
