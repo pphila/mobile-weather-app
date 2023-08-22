61096567b28d6f606ceb9bddc0ae31967d9713fc
pphila <efl2ice89@gmail.com>
2023-08-22 13:26:23 -0700

    add background image to component

--- src/components/UpcomingWeather.js
+++ src/components/UpcomingWeather.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, SafeAreaView, StyleSheet, FlatList, StatusBar } from 'react-native';
+import { View, Text, SafeAreaView, StyleSheet, FlatList, StatusBar, ImageBackground } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
 const DATA = [
@@ -63,12 +63,14 @@ const UpcomingWeather = () => {
   )
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Upcoming Weather</Text>
-      <FlatList 
-        data = {DATA} 
-        renderItem={renderItem}
-        keyExtractor={(item) => item.dt_txt}
-      />
+      <ImageBackground source={require('../../assets/upcoming-background.jpg')} style={styles.image}>
+        <Text>Upcoming Weather</Text>
+        <FlatList 
+          data = {DATA} 
+          renderItem={renderItem}
+          keyExtractor={(item) => item.dt_txt}
+        />
+      </ImageBackground>
     </SafeAreaView>
   )
 }
@@ -77,7 +79,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: StatusBar.currentHeight || 0,
-    backgroundColor: 'red'
+    backgroundColor: 'royalblue'
   },
   item: {
     padding: 20,
@@ -96,6 +98,9 @@ const styles = StyleSheet.create({
   date: {
     color: 'white',
     fontSize: 15
+  },
+  image: {
+    flex: 1
   }
 })
 export default UpcomingWeather;
