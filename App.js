e4d12ae02723c66ea08e93d9bc8f3a68da532180
pphila <efl2ice89@gmail.com>
2023-08-22 18:38:15 -0700

    add tab and tab screen feature to app.js

--- App.js
+++ App.js
@@ -1,18 +1,63 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import CurrentWeather from './src/components/CurrentWeather';
+import CurrentWeather from './src/screens/CurrentWeather';
+import UpcomingWeather from './src/screens/UpcomingWeather';
+import City from './src/screens/City';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Feather } from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator();
 
 const App = () => {
-  return (
-    <View style = {styles.container}>
-      <CurrentWeather />
-    </View>
+  return(
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{tabBarActiveTintColor: 'tomato', tabBarInactiveTintColor: 'gray'}}>
+          
+          <Tab.Screen 
+            name={'Current'} 
+            component={CurrentWeather} 
+            options={{
+              tabBarIcon: ({focused}) => (
+                <Feather 
+                  name={'droplet'} 
+                  size={25} 
+                  color={focused ? 'tomato' : 'black'} 
+                />
+              )
+            }}
+          />
+
+          <Tab.Screen 
+            name={'Upcoming'} 
+            component={UpcomingWeather} 
+            options={{
+              tabBarIcon: ({focused}) => (
+                <Feather 
+                  name={'clock'} 
+                  size={25} 
+                  color={focused ? 'tomato' : 'black'}
+                />
+              )
+            }}
+          />
+
+          <Tab.Screen 
+            name={'City'} 
+            component={City} 
+            options={{
+              tabBarIcon: ({focused}) => (
+                <Feather 
+                  name={'home'} 
+                  size={25}
+                  color={focused ? 'tomato' : 'black'}
+                />
+              )
+            }}
+          />
+          
+      </Tab.Navigator>
+    </NavigationContainer>
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  }
-})
 export default App;
