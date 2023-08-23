587beed97c25a9cb057e08bde2257c51710f4e96
pphila <efl2ice89@gmail.com>
2023-08-23 10:56:48 -0700

    add api call for weather data and data interpolation

--- App.js
+++ App.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { ActivityIndicator, View, StyleSheet } from 'react-native';
 import Tabs from './src/components/Tabs';
 import { NavigationContainer } from '@react-navigation/native';
+import { useGetWeather } from './src/hooks/useGetWeather';
 
 const App = () => {
-  return(
-    <NavigationContainer>
-      <Tabs />
-    </NavigationContainer>
+  const [loading, error, weather] = useGetWeather();
+  console.log(loading, error, weather);
+
+  if (weather && weather.list) {
+    return(
+      <NavigationContainer>
+        <Tabs weather={weather} />
+      </NavigationContainer>
+    )
+  }
+  
+  return (
+      <View style={styles.container}>
+        <ActivityIndicator size={'large'} color={'blue'} />
+      </View>
   )
-}
+};
+
+const styles= StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    flex: 1
+  }
+});
 
 export default App;
